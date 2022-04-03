8a0dbed86ac32dfff89e2b23013d0d833a4f1edb
Hugo Vinet <hugo.vinet@koober.com>
2022-04-03 10:55:41 +0200

    add blur on bg image

--- nativebase3/App.tsx
+++ nativebase3/App.tsx
@@ -72,6 +72,7 @@ const BgImage: FC = ({children}) => {
         uri: 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3388&q=80',
       }}
       resizeMode="cover"
+      blurRadius={3}
       style={StyleSheet.compose(
         styles.image,
         // @ts-ignore
