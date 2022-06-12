da1c9d10e64531fde598748f1d13db0b9997dff5
Hugo Vinet <hugo.vinet@koober.com>
2022-06-12 09:52:10 +0200

    move stuff

--- bareplayer/app/Home.tsx
+++ bareplayer/app/Home.tsx
@@ -1,53 +1,26 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {Button, Column, Image, Skeleton, Text} from 'native-base';
+import {Button, Column, Skeleton, Text, useColorMode} from 'native-base';
 import * as React from 'react';
-import {useState} from 'react';
 import {ParamList} from '../App';
 import {DarkModeSwitch} from '../design-system/DarkModeSwitch';
 
 export const Home = () => {
   const navigation = useNavigation<NavigationProp<ParamList>>();
 
+  const {colorMode} = useColorMode();
   return (
-    <Column alignItems="center" justifyContent="center" height="100%">
+    <Column
+      alignItems="center"
+      justifyContent="center"
+      height="100%"
+      bgColor={colorMode === 'dark' ? 'dark.50' : ''}>
       <DarkModeSwitch />
       <Text>Home Screen</Text>
-      <UsplashImage />
+      <Skeleton width={100} height={100} borderRadius={100} />
+
       <Button onPress={() => navigation.navigate('Player')}>
         Player Screen
       </Button>
     </Column>
-  );
-};
-
-const UsplashImage = () => {
-  const [unsplash, setUnsplash] = useState(null);
-  React.useEffect(() => {
-    fetch(
-      'https://api.unsplash.com/photos/random?client_id=fNFz6GGt9GLXFQjKD5XOC1zBvCqEevyM3xTlZjGbyDs',
-    )
-      .then(res => res.json())
-      .then(res => setUnsplash(res));
-  }, []);
-
-  // @ts-ignore
-  return unsplash?.height && unsplash?.width && unsplash?.urls?.small_s3 ? (
-    <>
-      <Image
-        // @ts-ignore
-        width={100}
-        // @ts-ignore
-        height={100}
-        resizeMode="center"
-        borderRadius={100}
-        source={{
-          // @ts-ignore
-          uri: unsplash?.urls?.small_s3,
-        }}
-        alt="unsplash"
-      />
-    </>
-  ) : (
-    <Skeleton width={100} height={100} borderRadius={100} />
   );
 };
--- bareplayer/design-system/DarkModeSwitch.tsx
+++ bareplayer/design-system/DarkModeSwitch.tsx
@@ -1,7 +1,8 @@
-import {Switch, useColorModeValue} from 'native-base';
+import {Switch, useColorMode} from 'native-base';
 import * as React from 'react';
 
 export const DarkModeSwitch = () => {
-  const {colorMode} = useColorModeValue() === 'dark';
-  return <Switch />;
+  const {colorMode, toggleColorMode} = useColorMode();
+  console.log(colorMode);
+  return <Switch onChange={toggleColorMode} />;
 };
